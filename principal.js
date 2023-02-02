6a1e3aa68d362e849a4be25fe202ed97e38eefb5
Jorge <jorgemateusguimaraespereira100@gmail.com>
2023-02-02 19:04:18 -0300

    Diminuindo o intervalo de troca de banner

--- principal.js
+++ principal.js
@@ -6,4 +6,5 @@ bannerAtual = (bannerAtual + 1) % 2;
 document.querySelector('h2#mensagem').textContent =
 banners[bannerAtual];
 }
-setInterval(trocaBanner, 2000);
+//Início do arquivo
+setInterval(trocaBanner, 1000);
